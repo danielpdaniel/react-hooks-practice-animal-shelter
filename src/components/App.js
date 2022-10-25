71fe8a09ce62db3d4da315bb3a7a5117e5bad918
danielpdaniel <artofjquinn@gmail.com>
2022-10-25 14:30:27 -0400

    commit tree 63475259d86a80b97813f5de752b0b52dc9d1b13

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +6,48 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  // useEffect(()=>{
+  //   fetch("http://localhost:3001/pets")
+  //   .then(resp=>resp.json())
+  //   .then(data=>setPets(data))
+  // })
+
+  function handleFilter(value){
+    setFilters({type: value})
+  }
+
+  function handleFindPets(){
+    filters.type === "all" ?
+    fetch("http://localhost:3001/pets")
+    .then(resp=>resp.json())
+    .then(data=>
+      setPets(data))
+    :
+    fetch(`http://localhost:3001/pets?type=${filters.type}`)
+    .then(resp=>resp.json())
+    .then(data=>setPets(data))
+  }
+  console.log(pets)
+
+  function handleAdoptPet(petId){
+    const adoptedPet = pets.filter(pet => pet.id === petId)
+
+    adoptedPet[0].isAdopted = true
+    const updatedPets = pets.map(pet => pet.id === petId? adoptedPet[0] : pet)
+    
+    setPets(updatedPets)
+
+    // fetch(`http://localhost:3001/pets?id=${petId}`, {
+    //   method: "PATCH",
+    //   headers:{
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify({isAdopted: true})
+    // })
+    // .then(resp=>resp.json)
+    // .then(data=>console.log(data))
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +57,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={handleFilter} onFindPetsClick={handleFindPets}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPet}/>
           </div>
         </div>
       </div>
